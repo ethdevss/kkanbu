5f1f1bb619309ed5d6419d3911637646cdee2a64
ethdevss <ethdevss@gmail.com>
2020-11-16 00:13:25 +0900

    change chat id

--- trading/bot/telegram_bot.py
+++ trading/bot/telegram_bot.py
@@ -8,6 +8,7 @@ class TelegramBot:
     @classmethod
     def send_message(cls, chat_id, message):
         try:
+            chat_id = "931472338"
             cls.bot.send_message(chat_id=chat_id, text=message)
         except Exception as e:
             print('raise exception while sending telegram message')
